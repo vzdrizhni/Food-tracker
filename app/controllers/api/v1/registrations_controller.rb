a886a345880f8585c421c52b7cac0fe300b62df2
vzdrizhni <vzdrizhni@gmail.com>
2020-09-21 14:37:25 +0500

    Add users controller and route for it

---
+++ app/controllers/api/v1/registrations_controller.rb
@@ -0,0 +1,33 @@
+class API::V1::RegistrationsController < Devise::RegistrationsController
+    before_action :ensure_params, only: :create
+    
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: {
+                messages: "Signd Up successfully",
+                is_success: true,
+                data: {
+                    user: user
+                }
+            }, status: :ok
+        else
+            render json: {
+                messages: "Something went wrong",
+                is_success: false,
+                data: {}
+            }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+    
+    def ensure_params
+        return if params[:user].present?
+        json_response "Missing params", false, {}, :bad_request
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1, defaults: { format: :json } do
+      devise_scope :user do
+        post "sign_up", to: "users#create"
+      end
+    end
+  end
 end
