743e57642a5641288866aea4d47f46b6e206c8f0
vzdrizhni <vzdrizhni@gmail.com>
2020-09-23 15:32:08 +0500

    Add another new property to the meal serializer

--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -1,5 +1,10 @@
 class MealSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :created
   belongs_to :user
   has_many :foods
+
+  def created
+    object.created_at.strftime("%d of %B, %Y")
+  end
+
 end
